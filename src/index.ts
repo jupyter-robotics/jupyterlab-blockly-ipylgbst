bbebe6f55843817cabe84887743cc6f8c8d15fcc
DenisaCG <denisa.checiu@gmail.com>
2023-04-24 18:02:09 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyterlab-blockly-ipylgbst extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-blockly-ipylgbst:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab-blockly-ipylgbst is activated!');
+  }
+};
+
+export default plugin;
